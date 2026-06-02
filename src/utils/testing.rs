b1e2487a1e48b28190bbbc845357920ff4364a8f
orizi <104711814+orizi@users.noreply.github.com>
2026-06-02 12:09:06 +0300

    Removed `include_` on test files - using load instead. (#1632)

--- src/utils/testing.rs
+++ src/utils/testing.rs
@@ -335,31 +335,6 @@ pub fn get_compiled_program(name: &str) -> (String, Program) {
     (entrypoint, program)
 }
 
-#[macro_export]
-macro_rules! include_program {
-    ( $path:expr ) => {
-        serde_json::from_str::<cairo_lang_sierra::program::VersionedProgram>(include_str!(concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/",
-            $path
-        )))
-        .unwrap()
-    };
-}
-#[macro_export]
-macro_rules! include_contract {
-    ( $path:expr ) => {
-        serde_json::from_str::<cairo_lang_starknet_classes::contract_class::ContractClass>(
-            include_str!(concat!(
-                env!("CARGO_MANIFEST_DIR"),
-                "/test_data_artifacts/",
-                $path
-            )),
-        )
-        .unwrap()
-    };
-}
-
 pub fn load_contract(path: &str) -> ContractClass {
     serde_json::from_str::<ContractClass>(
         &fs::read_to_string(format!(
